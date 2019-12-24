163408ea12aa6bdce00e93a256c33c0c30217c47
Evgen <banaykg@gmail.com>
2019-12-24 22:33:15 +0300

    vagrant work with frontend

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -43,7 +43,7 @@ return [
             ],
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+//            'errorAction' => 'site/error',
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
